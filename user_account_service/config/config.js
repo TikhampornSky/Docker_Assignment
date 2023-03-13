6f6eb3c960e3ae4009c0284bf98a08f35a89d19e
TikhampornSky <tikhamporntan@gmail.com>
2023-03-13 13:28:31 +0700

    fix: bugs

--- user_account_service/config/config.js
+++ user_account_service/config/config.js
@@ -6,7 +6,7 @@ module.exports = {
   db: {
     host: process.env.DATABASE_HOST || '127.0.0.1',
     database: 'ABCompany',
-    user: 'users_service',
+    user: 'root',
     password: '123',
     port: 3306
   }
--- user_account_service/repository/repository.js
+++ user_account_service/repository/repository.js
@@ -4,7 +4,7 @@
 //  a connected repository. Call 'disconnect' on this object when you're done.
 'use strict';
 
-var mysql = require('mysql');
+var mysql = require('mysql2');
 
 //  Class which holds an open connection to a repository
 //  and exposes some simple functions for accessing data.
